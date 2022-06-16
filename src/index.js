c061161a51ffb9cf5c55105bf2afb8f21ec0bf96
Clarisse Leduc <clarisseleduc@ukflooringdirect.co.uk>
2022-06-16 16:09:08 +0100

    Task 2 ongoing

--- src/index.js
+++ src/index.js
@@ -1,37 +1,44 @@
 // Create some JavaScript to manage the increment and decrement of the counter
 
-let productQuantity = document.getElementById("quantity");
-let quantity = 0;
-productQuantity.value = quantity;
+let product1Quantity = document.getElementById("quantity-1");
+let product2Quantity = document.getElementById("quantity-2");
+let decrement1 = document.getElementById("decrement-1");
+let increment1 = document.getElementById("increment-1");
+let decrement2 = document.getElementById("decrement-2");
+let increment2 = document.getElementById("increment-2");
+
+let quantity1 = 0;
+let quantity2 = 0;
+
+product1Quantity.value = quantity1;
+product2Quantity.value = quantity2;
 
 function calcultatePrice() {
   let product1Price = 100;
+  let product2Price = 50;
 
-  let price = document.getElementById("product-1-total-price");
-  let totalPrice = product1Price * quantity;
+  let price = document.getElementById("total-price");
+  let totalPrice = product1Price * quantity1 + product2Price * quantity2;
   price.textContent = `Total price: ${totalPrice}`;
 }
 
-function increase() {
-  if (quantity <= 9) {
-    quantity++;
-    productQuantity.value = quantity;
+function increase1() {
+  if (quantity1 <= 9) {
+    quantity1++;
+    product1Quantity.value = quantity1;
   }
 
   calcultatePrice();
 }
 
-function decrease() {
-  if (quantity >= 1) {
-    quantity--;
-    productQuantity.value = quantity;
+function decrease1() {
+  if (quantity1 >= 1) {
+    quantity1--;
+    product1Quantity.value = quantity1;
   }
 
   calcultatePrice();
 }
 
-let decrement = document.getElementById("decrement");
-let increment = document.getElementById("increment");
-
-decrement.addEventListener("click", decrease);
-increment.addEventListener("click", increase);
+decrement1.addEventListener("click", decrease1);
+increment1.addEventListener("click", increase1);
